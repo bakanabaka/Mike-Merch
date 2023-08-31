ac32b5e1d23e35f9a1df28a0ca3271ea99e265fb
Shravanth28 <shravanth.cs21@bmsce.ac.in>
2023-08-31 23:18:07 +0530

    mess

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
 const port = 3000;
 const Razorpay = require('razorpay');
 const cors = require('cors');
 // module.exports = app;
-
+app.use(express.static(__dirname));
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
 app.post("/payment", async (req, res) => {
     let { amount } = req.body;
     var instance = new Razorpay({ key_id: "rzp_test_vX3Y8QX16DXiF7", key_secret: "hK0d9r4Rp0hS50246uxQfqMM" });
